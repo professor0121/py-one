0b45ba852f21a4959aff2a61dfed0686075accf6
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-07-30 13:56:43 +0530

    intro is added

---
+++ Intro/hello.py
@@ -0,0 +1,12 @@
+print("hello world")
+# here the print is the built in function 
+
+# creator of python is Guido van Rossum
+# python is an interpreted language
+# python is dynamically typed
+# python is a high level programming language
+# python is an object oriented programming language
+# python is a multi-paradigm programming language
+# python is a general purpose programming language
+# python is an open source programming language
+# python is a cross platform programming language
